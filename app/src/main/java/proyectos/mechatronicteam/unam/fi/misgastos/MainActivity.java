61bcbe1cb274807b1cf90a0891050b410f297346
gustavo santos <gustavo.santos.nunez@gmail.com>
2018-05-27 23:41:05 -0500

    Interfaz 80%

--- app/src/main/java/proyectos/mechatronicteam/unam/fi/misgastos/MainActivity.java
+++ app/src/main/java/proyectos/mechatronicteam/unam/fi/misgastos/MainActivity.java
@@ -1,13 +1,24 @@
 package proyectos.mechatronicteam.unam.fi.misgastos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup radioGroup;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onClick(View view){
+        Intent miIntent= new Intent(MainActivity.this,AddGastoIngreso.class);
+        startActivity(miIntent);
+    }
+
 }
